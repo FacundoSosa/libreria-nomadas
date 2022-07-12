e2e7cb251ad0546778fd97ff25baad4a526991dd
Facundo <facundososa676@gmail.com>
2022-07-11 22:07:23 -0300

    desafio eventos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let opcion = prompt ("Ingrese una opción por su número: \n 1) Buscar un libro \n 2) Ver carrito \n 3) Registrarse \n 4) Catálogo")
+/* let opcion = prompt ("Ingrese una opción por su número: \n 1) Buscar un libro \n 2) Ver carrito \n 3) Registrarse \n 4) Catálogo")
 
 if (opcion == 1) {
     
@@ -61,14 +61,117 @@ if (opcion == 1) {
         {titulo: 'la odisea', autor: 'homero', precio: 590},
     ]
     
-    
     const catalogo = libros.map ((el) => el.titulo)
     
     alert(`Este es nuestro catálogo: ${catalogo}`);
     
 } else {
     alert ("Ingrese un número")
+} */
+
+/* function respuestaFiltro() {
+    filtrados = libros.filter(el => el.precio <= filtro)  
+
+    for (const libro of filtrados) {
+        let librosContainer = document.createElement("div");
+    
+        librosContainer.innerHTML = `<a href=""><img class="libros-portadas" src=${libro.img}></a>
+                                     <h3>${libro.titulo}</h3>   
+                                     <p>$${libro.precio}</p>   
+                                     <input class="botones-comprar" type="button" value="COMPRAR">`;
+    
+        librosRecomendados.append(librosContainer);                           
+    }
+} */
+
+
+
+
+function validarFormulario (e) {
+        e.preventDefault();
+        alert("Usuario registrado correctamente")
 }
+
+function respuestaFiltro1() {
+    filtrados = libros.filter(el => el.precio <= 400)
+
+    for (const libro of filtrados) {
+        let librosContainer = document.createElement("div");
+    
+        librosContainer.innerHTML = `<a href=""><img class="libros-portadas" src=${libro.img}></a>
+                                     <h3>${libro.titulo}</h3>   
+                                     <p>$${libro.precio}</p>   
+                                     <input class="botones-comprar" type="button" value="COMPRAR">`;
+    
+        librosRecomendados.append(librosContainer);                           
+    }
+}
+
+function respuestaFiltro2() {
+    filtrados = libros.filter(el => el.precio <= 800)
+
+    for (const libro of filtrados) {
+        let librosContainer = document.createElement("div");
+    
+        librosContainer.innerHTML = `<a href=""><img class="libros-portadas" src=${libro.img}></a>
+                                     <h3>${libro.titulo}</h3>   
+                                     <p>$${libro.precio}</p>   
+                                     <input class="botones-comprar" type="button" value="COMPRAR">`;
+    
+        librosRecomendados.append(librosContainer);                           
+    }
+}
+
+function respuestaFiltro3() {
+    filtrados = libros.filter(el => el.precio <= 1200)
+
+    for (const libro of filtrados) {
+        let librosContainer = document.createElement("div");
+    
+        librosContainer.innerHTML = `<a href=""><img class="libros-portadas" src=${libro.img}></a>
+                                     <h3>${libro.titulo}</h3>   
+                                     <p>$${libro.precio}</p>   
+                                     <input class="botones-comprar" type="button" value="COMPRAR">`;
+    
+        librosRecomendados.append(librosContainer);                           
+    }
+}
+
+const libros = [
+    {titulo: 'CUENTOS COMPLETOS', autor: 'edgar allan poe', precio: 1200, condicion: "nuevo", img: "img/libros-portadas/portada-eap.jpg"},
+    {titulo: 'EL ALEPH', autor: 'jorge luis borges', precio: 110, condicion: "nuevo", img: "img/libros-portadas/portada-el-aleph.png"},
+    {titulo: 'FINAL DEL JUEGO', autor: 'julio cortazar', precio: 460, condicion: "usado", img: "img/libros-portadas/portada-final-del-juego.jpg"},
+    {titulo: 'HISTORIAS DE CRONOPIOS Y DE FAMAS', autor: 'julio cortazar', precio: 320, condicion: "usado", img: "img/libros-portadas/portada-hcyf.jpg"},
+    {titulo: 'LA ILIADA', autor: 'homero', precio: 920, condicion: "nuevo", img: "img/libros-portadas/portada-la-iliada.jpg"},
+    {titulo: 'LA ODISEA', autor: 'homero', precio: 800, condicion: "nuevo", img: "img/libros-portadas/portada-la-odisea.png"},
+];
+
+
+let librosRecomendados = document.getElementById("librosRecomendados");
+
+const filtro1 = document.getElementById("filtro1")
+filtro1.addEventListener("click", respuestaFiltro1)
+
+const filtro2 = document.getElementById("filtro2")
+filtro2.addEventListener("click", respuestaFiltro2)
+
+const filtro3 = document.getElementById("filtro3")
+filtro3.addEventListener("click", respuestaFiltro3)
+
+
+let formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", validarFormulario)
+
+
+
+
+
+
+
+
+
+
 
 
 
